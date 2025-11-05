e5aaa6efd6985f2232457c8f374b6bdbfd2ec822
BrunoJubert <jubert.bruno@oclock.school>
2025-11-05 10:25:52 +0100

    favicon chemin corrigé

--- views/partials/header.php
+++ views/partials/header.php
@@ -35,12 +35,12 @@
     <meta name="twitter:image" content="https://www.emiliehedou.fr/public/assets/mimi1.jpg">
 
     <!-- /* Favicon */ -->
-    <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-    <link rel="shortcut icon" href="/favicon/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" href="/public/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/public/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="/public/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon/apple-touch-icon.png" />
     <meta name="apple-mobile-web-app-title" content="Emilie Hedou" />
-    <link rel="manifest" href="/favicon/site.webmanifest" />
+    <link rel="manifest" href="/public/favicon/site.webmanifest" />
 
     <!-- Fonts & Styles -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
